ddfb9645433f7f198043f7a6d28a593432eaf929
Jiayuan Zhang <forrestchang7@gmail.com>
2015-11-13 14:05:00 +0800

    homework 1

---
+++ homeworks/hw01/hw01.py
@@ -0,0 +1,131 @@
+from operator import add, sub
+
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    """
+    if b < 0:
+        f = a + (-b)
+    else:
+        f = a + b
+    return f
+
+
+def two_of_three(a, b, c):
+    """Return x*x + y*y, where x and y are the two largest members of the
+    positive numbers a, b, and c.
+
+    >>> two_of_three(1, 2, 3)
+    13
+    >>> two_of_three(5, 3, 1)
+    34
+    >>> two_of_three(10, 2, 8)
+    164
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    "*** YOUR CODE HERE ***"
+    def square(x):
+        return x * x
+
+    def sum_squares(x, y, z):
+        return square(x) + square(y) + square(z)
+
+    return sum_squares(a, b, c) - square(min(a, b, c))
+
+
+def largest_factor(n):
+    """Return the largest factor of n*n-1 that is smaller than n.
+
+    >>> largest_factor(4) # n*n-1 is 15; factors are 1, 3, 5, 15
+    3
+    >>> largest_factor(9) # n*n-1 is 80; factors are 1, 2, 4, 5, 8, 10, ...
+    8
+    """
+    "*** YOUR CODE HERE ***"
+    number = n * n - 1
+    for result in reversed(range(2, n)):
+        if number % result == 0:
+            return result
+
+
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and
+    false_result otherwise.
+
+    >>> if_function(True, 2, 3)
+    2
+    >>> if_function(False, 2, 3)
+    3
+    >>> if_function(3==2, 3+2, 3-2)
+    1
+    >>> if_function(3>2, 3+2, 3-2)
+    5
+    """
+    if condition:
+        return true_result
+    else:
+        return false_result
+
+def with_if_statement():
+    """
+    >>> with_if_statement()
+    1
+    """
+    if c():
+        return t()
+    else:
+        return f()
+
+def with_if_function():
+    return if_function(c(), t(), f())
+
+def c():
+    "*** YOUR CODE HERE ***"
+
+def t():
+    "*** YOUR CODE HERE ***"
+
+def f():
+    "*** YOUR CODE HERE ***"
+
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
+    length = 1
+    print(int(n))
+    while n != 1:
+        if n % 2 == 0:
+            n /= 2
+            print(int(n))
+            length += 1
+        else:
+            n = 3 * n + 1
+            print(int(n))
+            length += 1
+
+    return length
+
+
+challenge_question_program = """
+"*** YOUR CODE HERE ***"
+"""
+
